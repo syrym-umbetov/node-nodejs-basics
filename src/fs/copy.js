fc3a921d667b9bbf285cf78cbafe87a623a87b60
syrymumbetov <umbetov.syrym@gmail.com>
2022-06-04 00:16:45 +0600

    File system - copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,7 @@
 export const copy = async () => {
-    // Write your code here 
+  let fs = require('fs');
+  fs.copyFile('./files', './files_copy', function (err) {
+    if (err) throw err;
+    console.log('FS operation failed');
+  });
 };
