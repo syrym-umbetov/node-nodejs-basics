0350601ba7a5805bcdf2686749c374a7d170e792
syrymumbetov <umbetov.syrym@gmail.com>
2022-06-05 18:01:25 +0600

    compress decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,18 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
 export const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const gzip = createGzip();
+  const source = createReadStream('./files/fileToCompress.txt');
+  const destination = createWriteStream('archive.gz');
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
 };
+
+compress();
