ed2d171a8a792421ddd44cbafd5b3b2f8782cb9f
syrymumbetov <umbetov.syrym@gmail.com>
2022-06-05 16:46:24 +0600

    write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,8 @@
+import fs from 'fs';
 export const write = async () => {
-    // Write your code here 
+  // Write your code here
+  process.stdin.pipe(
+    fs.createWriteStream('./files/fileToWrite.txt', { flags: 'w' })
+  );
 };
+write();
